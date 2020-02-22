9a4bd3a1f2465da7aa5fca0fa15e45c55452f6e9
Diego Nogueira <diegoalvareznogueira@gmail.com>
2020-02-22 20:32:22 -0300

    options changed

--- zsh/.zsh.d/scripts/options.sh
+++ zsh/.zsh.d/scripts/options.sh
@@ -1,6 +1,6 @@
 unset MAILCHECK # disable "you have mail" warning
 
-os=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+# os=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
 # asdf
 . $HOME/.asdf/asdf.sh
@@ -25,11 +25,11 @@ export TERM="xterm-256color"
 # export STOW_DIR=$HOME/.dotfiles
 
 # if [[ -d "/usr/lib/jvm/java-8-openjdk-amd64" ]]; then
-  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-  export ANDROID_HOME=~/Android/Sdk
-  export PATH=$PATH:$ANDROID_HOME/platform-tools
-  export PATH=$PATH:$ANDROID_HOME/tools/bin
-  export PATH=$PATH:$ANDROID_HOME/emulator
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+# export ANDROID_HOME=~/Android/Sdk
+# export PATH=$PATH:$ANDROID_HOME/platform-tools
+# export PATH=$PATH:$ANDROID_HOME/tools/bin
+# export PATH=$PATH:$ANDROID_HOME/emulator
 # fi
 
 # if [[ -d "/usr/lib/jvm/java-10-openjdk" ]]; then
@@ -43,7 +43,7 @@ export TERM="xterm-256color"
 # fi
 
 # if [[ "$os" == "\"Ubuntu"\" ]]; then
-source /usr/share/doc/fzf/examples/key-bindings.zsh
+# source /usr/share/doc/fzf/examples/key-bindings.zsh
 # fi
 
 # if [[ "$os" == "\"Manjaro Linux\"" ]]; then
@@ -59,9 +59,9 @@ source ~/.config/tmuxinator/tmuxinator.zsh
 
 # WSL2 configurations
 
-# export $(dbus-launch)
-# export LIBGL_ALWAYS_INDIRECT=1
-# export WSL_VERSION=$(wsl.exe -l -v | grep -a '[*]' | sed 's/[^0-9]*//g')
-# export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
+export $(dbus-launch)
+export LIBGL_ALWAYS_INDIRECT=1
+export WSL_VERSION=$(wsl.exe -l -v | grep -a '[*]' | sed 's/[^0-9]*//g')
+export WSL_HOST=$(tail -1 /etc/resolv.conf | cut -d' ' -f2)
 # export ADB_SERVER_SOCKET=tcp:$WSL_HOST:5037
-# export DISPLAY=$WSL_HOST:0
+export DISPLAY=$WSL_HOST:0
